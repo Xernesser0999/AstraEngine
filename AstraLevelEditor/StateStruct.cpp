fd4f82910ee3d288c19543c61dc2329fae2e4533
Xern <steverpicx@gmail.com>
2026-03-10 11:31:37 +0100

    supr

--- AstraLevelEditor/StateStruct.cpp
+++
@@ -1,7 +0,0 @@
-#include "StateStruct.h"
-
-StateStruct::StateStruct() {};
-StateStruct::~StateStruct() {};
-
-void StateStruct::Update(float dt) {};
-void StateStruct::Render(sf::RenderWindow& window, Camera* camera) {};
